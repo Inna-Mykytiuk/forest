23bccadd46fccdff3464f0043738413795f7ed26
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-09-22 20:50:44 +0300

    fix

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -17,7 +17,7 @@ const Link = ({
 
   return (
     <AnchorLink
-    className={`${selectedPage === lowerCasePage? "text-mainGreen" : ""} 
+    className={`${selectedPage === lowerCasePage? "text-secondary500" : ""} 
     transition duration-300 ease-in-out hover:text-secondary500 `}
     href={`#${lowerCasePage}`}
     onClick={() => setSelectedPage(lowerCasePage)}
