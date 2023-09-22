713c61aa9c0009bcc578870183a5e33f47713371
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-09-22 18:20:57 +0300

    fix

--- src/scenes/benefits/Benefit.tsx
+++ src/scenes/benefits/Benefit.tsx
@@ -33,7 +33,7 @@ const Benefit = ({ icon, title, description, setSelectedPage }: Props) => {
         onClick={() => setSelectedPage(SelectedPage.ContactUs)}
         href={`#${SelectedPage.ContactUs}`}
       >
-        <p>Learn More</p>
+        {/* <p>Learn More</p> */}
       </AnchorLink>
     </motion.div>
   );
